12e509e51c2399ea85cb38d346d33d9570ff3329
ksawierprosyt <130401975+ksawierprosyt@users.noreply.github.com>
2024-03-13 23:16:31 +0000

    Update Rebirth Champions.lua

--- Rebirth Champions.lua
+++ Rebirth Champions.lua
@@ -1,43 +1,36 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+-- variables --
+getgenv().autoClick = false;
 
-local Window = Rayfield:CreateWindow({
-    Name = "Rebirth Champions X",
-    LoadingTitle = "Ksawier's Hub",
-    LoadingSubtitle = "by ksawierprosyt",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "Big Hub"
-    },
-    Discord = {
-       Enabled = true,
-       Invite = "https://discord.gg/ksawiergames", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-       RememberJoins = false -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = false, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "Untitled",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-    }
- })
+local RemoteEventPath = game:GetService("ReplicatedStorage").Events;
 
- local Tab1 = Window:CreateTab("Main") -- Title, Image
- local Tab2 = Window:CreateTab("Eggs")
- local Tab3 = Window:CreateTab("Player")
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
 
- local Slider = Tab3:CreateSlider({
-    Name = "WalkSpeed",
-    Range = {10, 100},
-    Increment = 1,
-    Suffix = "Speed",
-    CurrentValue = 10,
-    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-    game.Players.LocalPlayer.Character:SetAttribute("SpeedMultiplier",Value)
-    end,
- })
+local w = library:CreateWindow("Tapping Masters!")
+
+local a = w:CreateFolder("Main")
+
+local b = w:CreateFolder("Farm")
+
+a:DestroyGui()
+
+---Toggles
+
+b:Toggle("Auto Click",function(bool)
+    getgenv().autoClick = bool
+    print('Auto Click is:', bool);
+    if bool then
+        doClick();
+    end
+end)
+
+--Functions
+
+function doClick()
+    spawn(function ()
+        while autoClick == true do
+            local args = {[1] = 1}
+            game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Click3"):FireServer()
+            wait()
+           end
+        end)
+    end
